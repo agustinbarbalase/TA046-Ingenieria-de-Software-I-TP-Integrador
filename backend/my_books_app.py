eb17f6609f75c84506d2b37c4f71669e6e1248d5
jignacio14 <j14castro2001@gmail.com>
2024-11-05 19:22:18 -0300

    refactor: change definitions

--- backend/my_books_app.py
+++ backend/my_books_app.py
@@ -5,25 +5,25 @@ from typing import *
 class MyBooksApp:
 
     def __init__(self):
-        self.users = dict()
+        self.users_ids = dict()
 
-    def add_user(self, user: str):
-        self.users[user] = set()
+    def add_user(self, user_id: str):
+        self.users_ids[user_id] = set()
         return self
 
-    def has_user(self, user: str) -> bool:
-        return user in self.users
+    def has_user(self, user_id: str) -> bool:
+        return user_id in self.users_ids
 
-    def user_add_item(self, user: str, item: str):
-        if not user in self.users:
+    def user_add_item(self, user_id: str, item: str):
+        if not user_id in self.users_ids:
             return
-        self.users[user].add(item)
+        self.users_ids[user_id].add(item)
         return self
 
-    def user_has_item(self, user: str, item: str) -> bool:
-        if not user in self.users:
+    def user_has_item(self, user_id: str, item: str) -> bool:
+        if not user_id in self.users_ids:
             return False
-        return item in self.users[user]
+        return item in self.users_ids[user_id]
 
-    def get_user_shop_list(self, user: str) -> list:
-        return list(self.users[user])
+    def get_user_shop_list(self, user_id: str) -> list:
+        return list(self.users_ids[user_id])
