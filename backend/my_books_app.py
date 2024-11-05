bf97c05b258bfdb77f0a8ac52858d7ebac0c23c1
jignacio14 <j14castro2001@gmail.com>
2024-11-05 19:35:49 -0300

    feat: update app to use cart

--- backend/my_books_app.py
+++ backend/my_books_app.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from typing import *
 
+from shop_cart import ShopCart
+
 
 class MyBooksApp:
 
@@ -8,7 +10,9 @@ class MyBooksApp:
         self.users_ids = dict()
 
     def add_user(self, user_id: str):
-        self.users_ids[user_id] = set()
+        if not user_id in self.users_ids:
+            return self
+        self.users_ids[user_id] = ShopCart()
         return self
 
     def has_user(self, user_id: str) -> bool:
@@ -17,7 +21,7 @@ class MyBooksApp:
     def user_add_item(self, user_id: str, item: str):
         if not user_id in self.users_ids:
             return
-        self.users_ids[user_id].add(item)
+        self.users_ids[user_id].add_item(item, 1)
         return self
 
     def user_has_item(self, user_id: str, item: str) -> bool:
@@ -26,4 +30,13 @@ class MyBooksApp:
         return item in self.users_ids[user_id]
 
     def get_user_shop_list(self, user_id: str) -> list:
-        return list(self.users_ids[user_id])
+        if not user_id in self.users_ids:
+            return []
+        return self.users_ids[user_id].list_items()
+
+    def add_book_to_user(self, user_id: str, isbn: str, amount: int ):
+        if not user_id in self.users_ids:
+            return []
+        return self.users_ids[user_id].add_item(isbn, 1)
+            
+        
