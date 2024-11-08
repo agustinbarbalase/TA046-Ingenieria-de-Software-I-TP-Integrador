62218d17be3ed4978e10af66546662d9126fe990
jignacio14 <j14castro2001@gmail.com>
2024-11-08 19:59:25 -0300

    feata: add first two test definition

---
+++ backend/tests/auth_service_test.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+class AuthServiceTest(unittest.TestCase):
+
+    def test01_unsuccesfull_login(self):
+        pass
+    
+    def test02_succesfull_login(self):
+        pass
