cf239c95d9b8bbfca19bc9648c08bb3e88f9e73e
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-11-10 17:41:24 -0300

    Fixed `car` to `cart` in `ShopCartTest`

--- backend/tests/shop_cart_test.py
+++ backend/tests/shop_cart_test.py
@@ -16,24 +16,24 @@ class ShopCartTest(unittest.TestCase):
         self.catalog = set(list(self.item_name_one))
         self.item_non_catalog = "1984"
 
-        self.car = ShopCart()
+        self.cart = ShopCart()
 
     def test01_new_cart_is_empty(self):
-        self.assertTrue(self.car.is_empty())
+        self.assertTrue(self.cart.is_empty())
 
     def test02_can_add_item_to_cart(self):
-        self.car.add_item(self.item_name_one, 1)
+        self.cart.add_item(self.item_name_one, 1)
 
-        self.assertFalse(self.car.is_empty())
-        self.assertTrue(self.car.contains_item(self.item_name_one))
+        self.assertFalse(self.cart.is_empty())
+        self.assertTrue(self.cart.contains_item(self.item_name_one))
 
     def test03_can_add_multiple_items(self):
-        self.car.add_item(self.item_name_one, 1)
-        self.car.add_item(self.item_name_two, 1)
+        self.cart.add_item(self.item_name_one, 1)
+        self.cart.add_item(self.item_name_two, 1)
 
-        self.assertFalse(self.car.is_empty())
-        self.assertTrue(self.car.contains_item(self.item_name_one))
-        self.assertTrue(self.car.contains_item(self.item_name_two))
+        self.assertFalse(self.cart.is_empty())
+        self.assertTrue(self.cart.contains_item(self.item_name_one))
+        self.assertTrue(self.cart.contains_item(self.item_name_two))
 
     def test04_add_non_existing_cart_in_catalog_raises_error(self):
         car_with_catalog = ShopCart.with_catalog(self.catalog)
@@ -42,20 +42,20 @@ class ShopCartTest(unittest.TestCase):
             car_with_catalog.add_item(self.item_non_catalog, 1)
 
     def test05_list_cart_items(self):
-        self.car.add_item(self.item_name_one, 1)
-        self.car.add_item(self.item_name_two, 1)
+        self.cart.add_item(self.item_name_one, 1)
+        self.cart.add_item(self.item_name_two, 1)
 
-        self.assertFalse(self.car.is_empty())
+        self.assertFalse(self.cart.is_empty())
         self.assertEqual(
-            [(self.item_name_one, 1), (self.item_name_two, 1)], self.car.list_items()
+            [(self.item_name_one, 1), (self.item_name_two, 1)], self.cart.list_items()
         )
 
     def test06_add_existed_item_sum(self):
-        self.car.add_item(self.item_name_one, 2)
-        self.car.add_item(self.item_name_one, 3)
+        self.cart.add_item(self.item_name_one, 2)
+        self.cart.add_item(self.item_name_one, 3)
 
-        self.assertFalse(self.car.is_empty())
-        self.assertEqual([(self.item_name_one, 5)], self.car.list_items())
+        self.assertFalse(self.cart.is_empty())
+        self.assertEqual([(self.item_name_one, 5)], self.cart.list_items())
 
 
 if __name__ == "__main__":
