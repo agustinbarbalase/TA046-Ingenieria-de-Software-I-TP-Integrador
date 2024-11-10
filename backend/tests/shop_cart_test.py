606b17683bf01bba2a44ce342fb21cbecbef442a
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-11-10 16:50:07 -0300

    Defined a `Setup` for `ShopCartTest`

--- backend/tests/shop_cart_test.py
+++ backend/tests/shop_cart_test.py
@@ -9,67 +9,53 @@ from backend.domain.shop_cart import ShopCart, ItemNotInCatalog
 
 class ShopCartTest(unittest.TestCase):
 
+    def setUp(self):
+        self.item_name_one = "Cincuenta sombras de Alan key"
+        self.item_name_two = "El secreto de su codigo encapsulado"
+
+        self.catalog = set(list(self.item_name_one))
+        self.item_non_catalog = "1984"
+
+        self.car = ShopCart()
+
     def test01_new_cart_is_empty(self):
-        car = ShopCart()
-        self.assertTrue(car.is_empty())
+        self.assertTrue(self.car.is_empty())
 
     def test02_can_add_item_to_cart(self):
-        car = ShopCart()
-        new_item = ("cincuenta sombras de Alan key", 1)
+        self.car.add_item(self.item_name_one, 1)
 
-        car.add_item(new_item[0], new_item[1])
-
-        self.assertFalse(car.is_empty())
-        self.assertTrue(car.contains_item(new_item[0]))
+        self.assertFalse(self.car.is_empty())
+        self.assertTrue(self.car.contains_item(self.item_name_one))
 
     def test03_can_add_multiple_items(self):
-        car = ShopCart()
+        self.car.add_item(self.item_name_one, 1)
+        self.car.add_item(self.item_name_two, 1)
 
-        new_item_one = ("cincuenta sombras de Alan key", 1)
-        new_item_two = ("el secreto de su codigo encapsulado", 2)
-
-        car.add_item(new_item_one[0], new_item_one[1])
-        car.add_item(new_item_two[0], new_item_two[1])
-
-        self.assertFalse(car.is_empty())
-        self.assertTrue(car.contains_item(new_item_one[0]))
-        self.assertTrue(car.contains_item(new_item_two[0]))
+        self.assertFalse(self.car.is_empty())
+        self.assertTrue(self.car.contains_item(self.item_name_one))
+        self.assertTrue(self.car.contains_item(self.item_name_two))
 
     def test04_add_non_existing_cart_in_catalog_raises_error(self):
-        new_catalog = set()
-
-        name_item_in_catalog = "cincuenta sombras de Alan key"
-        name_item_random = "1984"
-
-        new_catalog.add(name_item_in_catalog)
-
-        car = ShopCart.with_catalog(new_catalog)
+        car_with_catalog = ShopCart.with_catalog(self.catalog)
 
         with self.assertRaises(ItemNotInCatalog):
-            car.add_item(name_item_random, 1)
+            car_with_catalog.add_item(self.item_non_catalog, 1)
 
     def test05_list_cart_items(self):
-        car = ShopCart()
+        self.car.add_item(self.item_name_one, 1)
+        self.car.add_item(self.item_name_two, 1)
 
-        new_item_one = ("cincuenta sombras de Alan key", 1)
-        new_item_two = ("el secreto de su codigo encapsulado", 2)
-
-        car.add_item(new_item_one[0], new_item_one[1])
-        car.add_item(new_item_two[0], new_item_two[1])
-
-        self.assertFalse(car.is_empty())
-        self.assertEqual([new_item_one, new_item_two], car.list_items())
+        self.assertFalse(self.car.is_empty())
+        self.assertEqual(
+            [(self.item_name_one, 1), (self.item_name_two, 1)], self.car.list_items()
+        )
 
     def test06_add_existed_item_sum(self):
-        car = ShopCart()
+        self.car.add_item(self.item_name_one, 2)
+        self.car.add_item(self.item_name_one, 3)
 
-        new_item = "cincuenta sombras de Alan key"
-
-        car.add_item(new_item, 2)
-        car.add_item(new_item, 3)
-
-        self.assertFalse(car.is_empty())
-        self.assertEqual([(new_item, 5)], car.list_items())
+        self.assertFalse(self.car.is_empty())
+        self.assertEqual([(self.item_name_one, 5)], self.car.list_items())
 
 
 if __name__ == "__main__":
