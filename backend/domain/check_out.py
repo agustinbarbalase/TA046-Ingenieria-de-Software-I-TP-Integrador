16a7889773c2dc803ad500d5c2a310589a6309b8
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-11-12 19:34:58 -0300

    Added Checkout

---
+++ backend/domain/check_out.py
@@ -0,0 +1,47 @@
+from datetime import date
+
+
+class CheckOut:
+
+    def __init__(self):
+        pass
+
+    def initialize_date(self, date):
+        self.month = date
+
+    @classmethod
+    def empty_cart_message_error(cls):
+        return "Empty cart"
+
+    @classmethod
+    def invalid_date_message_error(cls):
+        return "Invalid date"
+
+    @classmethod
+    def expired_card_message_error(cls):
+        return "Expired card"
+
+    @classmethod
+    def with_date(cls, date):
+        return cls().initialize_date(date)
+
+    def _check_date(self, date):
+        try:
+            month, year = date[:2], date[2:]
+            expiration_date = date(int(year), int(month), 1)
+            return expiration_date
+        except ValueError:
+            raise Exception(CheckOut.invalid_date_message_error())
+
+    def _check_empty_cart(self, cart):
+        if cart.is_empty():
+            raise Exception(CheckOut.empty_cart_message_error())
+
+    def _check_expired(self, date):
+        if date <= datetime.now():
+            raise Exception(CheckOut.expired_card_message_error())
+
+    def check_out(self, cart, card):
+        # expiration_date = self._check_date(card["card_expiration_date"])
+        # self._check_expired(expiration_date)
+        self._check_empty_cart(cart)
---
+++ backend/tests/check_out_test.py
@@ -0,0 +1,72 @@
+import sys
+import os
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from domain.check_out import CheckOut
+from domain.shop_cart import ShopCart
+
+
+class CheckOutTest(unittest.TestCase):
+
+    def test_checkout_with_empty_cart(self):
+        card_number = "0101010101010101"
+        card_expiration_date = "112028"
+        card_code = "420"
+
+        cart = ShopCart()
+        check_out = CheckOut()
+
+        with self.assertRaises(Exception) as context:
+            check_out.check_out(
+                cart,
+                {
+                    "card_number": card_number,
+                    "card_expiration_date": card_expiration_date,
+                    "card_code": card_code,
+                },
+            )
+
+        print(context.exception)
+        self.assertEqual(str(context.exception), CheckOut.empty_cart_message_error())
+
+    # def test_invalid_date(self):
+    #     card_number = "0101010101010101"
+    #     invalid_card_expiration_date = "142024"
+    #     card_code = "420"
+
+    #     cart = ShopCart()
+    #     check_out = CheckOut()
+
+    #     with self.assertRaises(Exception) as context:
+    #         check_out.check_out(
+    #             cart,
+    #             {
+    #                 "card_number": card_number,
+    #                 "card_expiration_date": invalid_card_expiration_date,
+    #                 "card_code": card_code,
+    #             },
+    #         )
+
+    #     self.assertEqual(str(context.exception), CheckOut.invalid_date_message_error())
+
+    # def test_checkout_with_expired_card(self):
+    #     card_number = "0101010101010101"
+    #     invalid_card_expiration_date = "112020"
+    #     card_code = "420"
+
+    #     cart = ShopCart()
+    #     check_out = CheckOut.with_date("112024")
+
+    #     with self.assertRaises(Exception) as context:
+    #         check_out.check_out(
+    #             cart,
+    #             {
+    #                 "card_number": card_number,
+    #                 "card_expiration_date": invalid_card_expiration_date,
+    #                 "card_code": card_code,
+    #             },
+    #         )
+
+    #     self.assertEqual(str(context.exception), CheckOut.expired_card_message_error())
