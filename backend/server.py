5e24238a8f07088ab5831250555913a8a7e32bda
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-11-12 16:36:14 -0300

    Added methods for backend

--- backend/server.py
+++ backend/server.py
@@ -13,7 +13,7 @@ class TusLibrosWebServer:
         self.rest_interface = RestInterface()
         CORS(self.app, origins=["http://localhost:3000"], supports_credentials=True)
 
-        @self.app.route("/createCart")
+        @self.app.route("/createCart", methods=["GET"])
         def create_cart():
             params = request.args.to_dict()
             response = self.rest_interface.create_cart(
@@ -21,13 +21,13 @@ class TusLibrosWebServer:
             )
             return response["body"]
 
-        @self.app.route("/listCart")
+        @self.app.route("/listCart", methods=["GET"])
         def list_cart():
             params = request.args.to_dict()
             response = self.rest_interface.list_cart(params["userId"])
             return response["body"]
 
-        @self.app.route("/addToCart")
+        @self.app.route("/addToCart", methods=["GET"])
         def add_to_cart():
             params = request.args.to_dict()
             response = self.rest_interface.add_to_cart(
