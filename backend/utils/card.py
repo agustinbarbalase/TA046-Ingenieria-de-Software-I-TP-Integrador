3c76587dd6ed5864b57fb5c862d6f293078bfce3
Jignacio14 <j14castro2001@gmail.com>
2024-11-12 22:26:49 -0300

    feat: add card impl

--- backend/utils/card.py
+++ backend/utils/card.py
@@ -1,8 +1,8 @@
-# class Card:
+class Card:
 
-#     def __init__(self, number, gregorian_month_of_year):
-#         self.number = number
-#         self.gregorian_month_of_year = gregorian_month_of_year
+    def __init__(self, number, gregorian_month_of_year):
+        self.number = number
+        self.gregorian_month_of_year = gregorian_month_of_year
 
-#     def is_expired(self):
-#         return self.gregorian_month_of_year.is_greater_than_today()
+    def is_expired(self):
+        return self.gregorian_month_of_year.is_greater_than_today()
