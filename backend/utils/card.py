13dc28b92f3a2850375d454316c53c2d6548f8ba
Jignacio14 <j14castro2001@gmail.com>
2024-11-12 22:25:47 -0300

    feat: test gregorian_month_of_year are broken

---
+++ backend/utils/card.py
@@ -0,0 +1,8 @@
+# class Card:
+
+#     def __init__(self, number, gregorian_month_of_year):
+#         self.number = number
+#         self.gregorian_month_of_year = gregorian_month_of_year
+
+#     def is_expired(self):
+#         return self.gregorian_month_of_year.is_greater_than_today()
