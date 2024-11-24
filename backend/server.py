3ef50fb20d404e07a5bf54e01b18ae03b9e046d1
Agustin Barbalase <abarbalase@fi.uba.ar>
2024-11-24 17:49:52 -0300

    For init app you need pass `Catalog` and `Users`

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
-from datetime import datetime
 import sys
+import json
+
+from datetime import datetime
 from flask import Flask, request
 from typing import *
 
@@ -11,22 +13,15 @@ from domain.auth.auth_service import AuthService
 
 
 class TusLibrosWebServer:
-    def __init__(self):
+    def __init__(self, catalog: dict[str, str], users: dict[str, str]):
         self.flask_app = Flask(__name__)
-        self.auth = AuthService.with_users({})
-        self.catalog = {
-            "9780137314942": "π",  # π = 3.14159
-            "9780321278654": "e",  # e = 2.71828
-            "9780201710915": "φ",  # φ = 1.61803
-            "9780321125217": "γ",  # γ = 0.57721
-            "9780735619654": "c",  # c = 299,792,458 m/s
-            "9780321146533": "g",  # g = 9.80665 m/s²
-        }
-        self.app = MyBooksApp.with_catalog_and_auth(self.catalog, self.auth)
-        self.rest_interface = RestInterface.with_app(self.app)
         CORS(
             self.flask_app, origins=["http://localhost:3000"], supports_credentials=True
         )
+
+        self.auth = AuthService.with_users(users)
+        self.app = MyBooksApp.with_catalog_and_auth(catalog, self.auth)
+        self.rest_interface = RestInterface.with_app(self.app)
 
         @self.flask_app.route("/createCart", methods=["GET"])
         def create_cart():
@@ -60,7 +55,55 @@ class TusLibrosWebServer:
         self.flask_app.run(port=port)
 
 
+def open_json_file(file_path: str, closure):
+    try:
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            return closure(data)
+    except FileNotFoundError as err:
+        raise Exception(f"The file {file_path} does not exist.")
+    except json.JSONDecodeError as err:
+        raise Exception("The file is not a valid JSON.")
+    except Exception as e:
+        raise Exception(e)
+
+
 if __name__ == "__main__":
-    port = int(sys.argv[1])
-    server = TusLibrosWebServer()
-    server.listening_on(port)
+    try:
+        if len(sys.argv) != 4:
+            raise Exception(
+                "Abstent params: For run program needs:\n"
+                "    <port>: The number of port to raise app\n"
+                "    <catalog_file_path>: The path where is saved the catalog.\n"
+                "    The format is a JSON with a list with dict. The dict have:\n"
+                '    "isbn" and "price"\n'
+                "    <users_file_path>: The path where is saved the users.\n"
+                "    The format is a JSON with a list with dict. The dict have:\n"
+                '    "user_id" and "passoword"\n'
+            )
+
+        port, catalog_file_path, users_file_path = (
+            int(sys.argv[1]),
+            sys.argv[2],
+            sys.argv[3],
+        )
+
+        def closure_for_catalog(catalog_file):
+            catalog: dict[str, str] = {}
+            for book in catalog_file:
+                catalog[book["isbn"]] = book["price"]
+            return catalog
+
+        def closure_for_users(users_file):
+            users: dict[str, str] = {}
+            for user in users_file:
+                users[user["user_id"]] = user["password"]
+            return users
+
+        catalog = open_json_file(catalog_file_path, closure_for_catalog)
+        users = open_json_file(users_file_path, closure_for_users)
+
+        server = TusLibrosWebServer(catalog, users)
+        server.listening_on(port)
+    except Exception as err:
+        print(f"{err}")
